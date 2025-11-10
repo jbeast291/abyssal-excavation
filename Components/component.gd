f2598239feb654889a2206b6dbdcde142cd087d4
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2025-11-09 21:01:06 -0800

    fix unused variable warnings

--- Components/component.gd
+++ Components/component.gd
@@ -2,7 +2,7 @@
 class_name Component extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-func update(delta: float) -> void:
+func update(_delta: float) -> void:
 	pass
-func physics_update(delta: float) -> void:
+func physics_update(_delta: float) -> void:
 	pass
